88ab5c4c1410475aa53ee0d9518d7d3f337dc430
Tarik Mahmood <mhmtarik@users.noreply.github.com>
2021-01-28 18:45:37 -0500

    stage data and check data shape

--- youtubeanalyze.py
+++ youtubeanalyze.py
@@ -3,5 +3,15 @@ import pandas as pd
 import seaborn as sns
 import numpy as nm
 import plotly.express as px
+import seaborn as sns
+sns.set(style='darkgrid')
 
 # %%
+# Importing the data source
+ca = pd.read_csv("C:/Users/tarik.mahmood/Documents/PYTHON/youtubeCAtrending/CAvideos.csv")
+
+# %%
+# Checking data cols & rows
+ca.shape
+# %%
+# Importing the data source
