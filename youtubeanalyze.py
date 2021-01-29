251af58307188409c3691f8f3c50b3db22509946
Tarik Mahmood <mhmtarik@users.noreply.github.com>
2021-01-28 20:29:24 -0500

    format 2 datetime col

--- youtubeanalyze.py
+++ youtubeanalyze.py
@@ -20,3 +20,19 @@ ca.columns
 # Getting rid of redundant cols.
 ca.drop(['video_id','thumbnail_link', 'description'], axis=1, inplace=True)
 # %%
+# Checking data types
+ca.dtypes
+# %%
+# Checking 2 cols types that need to be updated
+ca[['trending_date','publish_time']].head()
+# %%
+# Update publish time to datetime
+ca['publish_time'] = ca['publish_time'].astype('datetime64[ns]')
+
+# %%
+# Append '20' in front then reformat trending date & set proper date col order
+ca['trending_date'] = ['20'] + ca['trending_date']
+ca['trending_date'] = pd.to_datetime(ca['trending_date'], format = "%Y.%d.%m")
+# %%
+
+# %%
